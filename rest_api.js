da25fe9d3a9632a7e7ca504423689297336d4e0a
biryo <49580251+biryo@users.noreply.github.com>
2020-04-23 16:28:38 +0700

    Update rest_api.js

--- rest_api.js
+++ rest_api.js
@@ -479,7 +479,7 @@ function search_plesetan(){
 $('#dta_plesetan').html("");
 
     $.ajax({
-      url: 'https://f681c6fd.ngrok.io/sw/db_spell.php',
+      url: 'http://74ed4ac1.ngrok.io/sw/db_spell.php',
       type:'get',
       dataType:'json',
       data:{
